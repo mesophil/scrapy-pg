4d9db504fa7721b0bab3d3f98446d323106339c9
mesophil <spongiestmesophyll@gmail.com>
2024-06-09 20:56:49 -0600

    Price and image fetch

--- uni_scraper/uni_scraper/spiders/uni_spider.py
+++ uni_scraper/uni_scraper/spiders/uni_spider.py
@@ -5,20 +5,16 @@ class UniSpiderSpider(scrapy.Spider):
     name = "uni_spider"
 
     def start_requests(self):
-        # wait until elements with a div that has class info are rendered
         url = "https://www.uniqlo.com/ca/en/men/tops/tops-collections"
         yield scrapy.Request(url, meta={"playwright": True,
                                         }
                                         )
 
     def parse(self, response):
-        # yield {"url": response.url}
-
-        # product_item = UniItem()
+        product_item = UniItem()
 
         for product in response.css("div.fr-product-card.default"):
-            # product_item['name'] = product.css("h2.description.decscription-text.fr-no-uppercase::text").get()
-            # product_item['price'] = 0
-            # product_item['url'] = "a"
-            # yield product_item
-            yield {'name' : product.css("h2.description.decscription-text.fr-no-uppercase::text").get()}
+            product_item['name'] = product.css(".description.fr-no-uppercase::text").get()
+            product_item['price'] = product.css("span.fr-price-currency>span::text").get()
+            product_item['image'] = product.css("img.thumb-img::attr(src)").get()
+            yield product_item
