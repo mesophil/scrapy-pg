73190b65082489f6c703a0333f5a2fb05a326c32
mesophil <spongiestmesophyll@gmail.com>
2024-06-10 18:08:21 -0600

    URL lists

---
+++ uni_scraper/uni_scraper/generate_urls.py
@@ -0,0 +1,13 @@
+def read_urls(file_name):
+    urls = []
+
+    with open(file_name, 'r') as file:
+        for url in file:
+            urls.append(url.rstrip())
+    return urls
+
+urls = ['https://www.uniqlo.com/ca/en/men/bottoms/bottoms-collections',
+        'https://www.uniqlo.com/ca/en/men/tops/tops-collections',
+        'https://www.uniqlo.com/ca/en/men/accessories-and-home/accessories-collections',
+        'https://www.uniqlo.com/ca/en/men/outerwear/outerwear-collections',
+        ]
