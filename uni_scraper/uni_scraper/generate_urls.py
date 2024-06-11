37694d449a3e74a2bb75f08b108e0cb12b5c4ab5
mesophil <spongiestmesophyll@gmail.com>
2024-06-10 18:43:21 -0600

    Load all test

--- uni_scraper/uni_scraper/generate_urls.py
+++ uni_scraper/uni_scraper/generate_urls.py
@@ -7,7 +7,7 @@ def read_urls(file_name):
     return urls
 
 urls = ['https://www.uniqlo.com/ca/en/men/bottoms/bottoms-collections',
-        'https://www.uniqlo.com/ca/en/men/tops/tops-collections',
-        'https://www.uniqlo.com/ca/en/men/accessories-and-home/accessories-collections',
-        'https://www.uniqlo.com/ca/en/men/outerwear/outerwear-collections',
+        # 'https://www.uniqlo.com/ca/en/men/tops/tops-collections',
+        # 'https://www.uniqlo.com/ca/en/men/accessories-and-home/accessories-collections',
+        # 'https://www.uniqlo.com/ca/en/men/outerwear/outerwear-collections',
         ]
--- uni_scraper/uni_scraper/spiders/uni_spider.py
+++ uni_scraper/uni_scraper/spiders/uni_spider.py
@@ -1,17 +1,36 @@
 import scrapy
 from uni_scraper.items import UniItem
 from uni_scraper.generate_urls import urls
+from scrapy_playwright.page import PageMethod
 
 class UniSpiderSpider(scrapy.Spider):
     name = "uni_spider"
 
     def start_requests(self):
         for url in urls:
-            yield scrapy.Request(url, meta={"playwright": True})
+            yield scrapy.Request(url, 
+                                 meta={"playwright": True, 
+                                       "playwright_include_page": True,
+                                       }, 
+                                 callback=self.parse)
 
-    def parse(self, response):
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
+        page.set_default_timeout(1000)
+
+        try:
+            # maybe use class fr-load-more
+            # but then how to target the <a>? the div is inside the link rather than containing it
+            # problem right now is that w12 might be used elsewhere lol
+            while button := page.locator("//div[contains(@class,'w12')]/a"):
+                await button.scroll_into_view_if_needed()
+                await button.click()
+        except:
+            pass
+
+        # await page.close()
+
         product_item = UniItem()
-
         for product in response.css("div.fr-product-card.default"):
             product_item['name'] = product.css(".description.fr-no-uppercase::text").get()
             product_item['price'] = product.css("span.fr-price-currency>span::text").get()
